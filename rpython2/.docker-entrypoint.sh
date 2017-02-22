632781f3cdee4db8d17d283ce40ce7cc494a5bf5
Joel Kim <kim.dohhyoung@gmail.com>
2017-02-22 14:58:54 +0900

    rstudio 로그인 문제 해결

--- rpython2/.docker-entrypoint.sh
+++ rpython2/.docker-entrypoint.sh
@@ -3,11 +3,15 @@
 # delete password
 unset USER_PASS
 
-# start supervisord
-service supervisor start 
+# start syslog
+service rsyslog start
 
 # start ssh
 service ssh start
+
+# start supervisord
+#service supervisor start 
+su --login dockeruser -c "/usr/bin/supervisord"
 
 # user login
 sudo -u $USER_ID -i '/bin/bash'

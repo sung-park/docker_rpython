402c68888a43070000cf135f2520f3a3837b6334
Joel Kim <kim.dohhyoung@gmail.com>
2018-08-21 20:17:05 +0900

    00.py startup file 수정.
    warning 무시

--- rpython/00.py
+++ rpython/00.py
@@ -1,11 +1,11 @@
+import warnings
+warnings.simplefilter('default')
+
 import numpy as np
 import scipy as sp
 import pandas as pd
 import statsmodels.api as sm
-import statsmodels.formula.api as smf
-import statsmodels.stats.api as sms
 import sklearn as sk
-
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pylab as plt

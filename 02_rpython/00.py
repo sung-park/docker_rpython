332cd6e9bb79faaf0d78ba80b59081d489882824
Joel Kim <kim.dohhyoung@gmail.com>
2018-10-30 14:54:33 +0900

    디렉토리 이름 변경 및 우분투 버전 18.04 업그레이드

---
+++ 02_rpython/00.py
@@ -0,0 +1,11 @@
+import warnings
+warnings.simplefilter('ignore')
+
+import matplotlib as mpl
+mpl.use('Agg')
+
+import seaborn as sns
+sns.set()
+sns.set_style("whitegrid")
+sns.set_color_codes()
+

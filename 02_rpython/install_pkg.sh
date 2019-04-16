fbc78d8b14e9cfe2607ca901a539edca443ada24
Joel Kim <kim.dohhyoung@gmail.com>
2019-04-16 09:33:56 +0900

    pytorch-cpu

--- 02_rpython/install_pkg.sh
+++ 02_rpython/install_pkg.sh
@@ -2,6 +2,6 @@
 cat pkgs_conda.txt       | paste -sd " " - | xargs conda install --yes
 cat pkgs_conda-forge.txt | paste -sd " " - | xargs conda install --yes -c conda-forge
 conda install --yes -c ioam    geoviews
-conda install --yes -c pytorch pytorch torchvision
+conda install --yes -c pytorch pytorch-cpu torchvision-cpu
 cat pkgs_pip.txt | paste -sd " " - | xargs pip install
 conda clean --yes --all

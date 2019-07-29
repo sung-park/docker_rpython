406e5adc5f3a66494e5e78354ed574dd5a7cb929
joel kim <kim.dohhyoung@gmail.com>
2019-07-29 10:53:30 +0900

    upgrade anaconda to 2019.07

--- 02_rpython/install_pkg.sh
+++ 02_rpython/install_pkg.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-cat pkgs_conda.txt       | paste -sd " " - | xargs conda install --yes
+echo "reinstall conda to 4.6.14..."
+conda install conda=4.6.14 --yes --force-reinstall
+echo "install conda packages..."
+cat pkgs_conda.txt | paste -sd " " - | xargs conda install --yes
+echo "reinstall conda to latest..."
+conda update conda -c conda-canary --yes --force-reinstall
+echo "install conda-forge packages..."
 cat pkgs_conda-forge.txt | paste -sd " " - | xargs conda install --yes -c conda-forge
-conda install --yes -c ioam    geoviews
-conda install --yes -c pytorch pytorch-cpu torchvision-cpu
+echo "install pip packages..."
 cat pkgs_pip.txt | paste -sd " " - | xargs pip install --progress-bar off
+echo "install cleaning..."
 conda clean --yes --all

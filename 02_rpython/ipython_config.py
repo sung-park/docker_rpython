873e45d9afe2ca18a35dfd017bef00a23bb2a8da
Joel Kim <kim.dohhyoung@gmail.com>
2020-01-27 13:19:42 +0900

    %matplotlib line 문제해결

--- 02_rpython/ipython_config.py
+++ 02_rpython/ipython_config.py
@@ -1,6 +1,8 @@
 c = get_config()
 
 c.InteractiveShellApp.exec_lines = [
+    "%matplotlib inline",
+    "%autoreload 2",
     "mpl.rc('font', family='nanumgothic')",
     "mpl.rc('axes', unicode_minus=False)",
     "mpl.rc('figure', figsize=(8, 5))",
